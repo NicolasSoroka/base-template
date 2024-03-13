9d7728a0f7bde23658670e93479a420f251cbac6
Nicolas Soroka <nsoroka@quadiontech.com>
2024-03-13 11:47:39 -0300

    primereact + tailwind

---
+++ src/Test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'primereact/button';
+
+const Test = () => {
+  return (
+    <div>
+      <h2>test primereact</h2>
+      <Button label="Button" severity='danger' className=''/>
+    </div>
+  )
+}
+
+export default Test
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PrimeReactProvider } from "primereact/api";
+import Tailwind from "primereact/passthrough/tailwind";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import Test from "./Test";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+      {/* <ThemeSwitcher /> */}
+      <Test />
+    </PrimeReactProvider>
   </React.StrictMode>,
-)
+);
+
+
+
