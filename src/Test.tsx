bd816edc0bf54182c53c90bda0f53cda3d19449a
Nicolas Soroka <nsoroka@quadiontech.com>
2024-03-14 19:37:59 -0300

    added base router

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Button } from 'primereact/button';
+import React from "react";
+import { Button } from "primereact/button";
+import "primeicons/primeicons.css";
 
 const Test = () => {
   return (
     <div>
       <h2>test primereact</h2>
-      <Button label="Button" severity='danger' className=''/>
+      <Button icon="pi pi-check" />
+      <Button label="Submit" icon="pi pi-check" />
+      <Button label="Submit" icon="pi pi-check" iconPos="right" />
+      <i className="pi pi-spin pi-spinner" style={{ fontSize: "2rem" }}></i>
+      <i className="pi pi-spin pi-cog" style={{ fontSize: "2rem" }}></i>
     </div>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
