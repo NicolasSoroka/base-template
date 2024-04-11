01a63e9eb0b45f2e949e250af8e390088c82d928
Nicolas Soroka <nsoroka@quadiontech.com>
2024-04-10 22:54:21 -0300

    test

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 const Layout = ({ children }) => {
   return (
     <div className="container mx-auto flex h-screen flex-col justify-between">
-      <header>Cambios en el template</header>
+      <header>header</header>
       <main className="flex h-full">{children}</main>
       <footer>test footer</footer>
     </div>
