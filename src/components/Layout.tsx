c123bd2b35ccdb977bf88121b309e05d21a1d69d
Nicolas Soroka <nsoroka@quadiontech.com>
2024-04-10 22:59:46 -0300

    asdf

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 const Layout = ({ children }) => {
   return (
     <div className="container mx-auto flex h-screen flex-col justify-between">
-      <header>header</header>
+      <header>check</header>
       <main className="flex h-full">{children}</main>
       <footer>test footer</footer>
     </div>
